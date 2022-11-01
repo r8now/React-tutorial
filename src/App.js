b42c9f92cb4d3e8ad20ae055d17f5bfbfbf24f70
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2022-11-01 18:02:38 +0100

    removed some files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+    <Job salary={9000} position="Senior"/>
+
+    </div>
+  );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
     </div>
   );
 }
